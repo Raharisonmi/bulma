b83068c974568ad8e0e6714f2f0e242a0a00a5a7
Mihaja <raharison05@gmail.com>
2023-11-06 19:08:58 +0300

    navbar humberger

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+
+function menu(){
+    a = document.getElementById("menubas");
+
+    if ( a.style.display == "block"){
+        a.style.display = "none";
+    }
+    else{
+        a.style.display = "block";
+    }
+}
